34652e5c475dec06e9ce1c130dd504c93efe7c14
trykea <tikearith@gmail.com>
2024-12-24 15:06:46 +0700

    added validation to the input and passing data in the current screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -9,7 +9,19 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   bool _isLogin = true;
+  var enteredEmail = '';
+  var enteredPassword = '';
+  final _form = GlobalKey<FormState>();
 
+  void _submit() {
+   final isValid = _form.currentState!.validate();
+   if (isValid) {
+     _form.currentState!.save();
+     print(enteredEmail);
+     print(enteredPassword);
+   }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +47,7 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
+                      key: _form,
                         child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
@@ -45,22 +58,36 @@ class _AuthScreenState extends State<AuthScreen> {
                           keyboardType: TextInputType.emailAddress,
                           autocorrect: false,
                           textCapitalization: TextCapitalization.none,
+                          validator: (value){
+                            if (value == null || value.trim().isEmpty || !value.contains('@')) {
+                              return 'Please enter a valid email address';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            enteredEmail = value!;
+                          },
                         ),
                         TextFormField(
                           decoration: const InputDecoration(
                             labelText: 'Password',
                           ),
                           obscureText: true,
+                          validator: (value){
+                            if (value == null || value.trim().length <6) {
+                              return 'Password must be at least characters long';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            enteredPassword = value!;
+                          },
                         ),
                         SizedBox(
                           height: 12,
                         ),
                         ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin = !_isLogin;
-                              });
-                            },
+                            onPressed: _submit,
                             style: ElevatedButton.styleFrom(
                                 backgroundColor: Theme.of(context)
                                     .colorScheme
