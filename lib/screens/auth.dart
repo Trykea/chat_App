7e9e77096941b8dec3484e9bcd3cb8289f0e69fb
trykea <tikearith@gmail.com>
2024-12-27 11:58:27 +0700

    added function to sign up user

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -13,15 +16,30 @@ class _AuthScreenState extends State<AuthScreen> {
   var enteredPassword = '';
   final _form = GlobalKey<FormState>();
 
-  void _submit() {
-   final isValid = _form.currentState!.validate();
-   if (isValid) {
-     _form.currentState!.save();
-     print(enteredEmail);
-     print(enteredPassword);
-   }
+  void _submit() async {
+    final isValid = _form.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState!.save();
+    if (_isLogin) {
 
+    }  else{
+      try{
+      final userCredentials = await _firebase.createUserWithEmailAndPassword(email: enteredEmail, password: enteredPassword);
+
+      } on FirebaseAuthException catch(error) {
+        if (error.code == 'email-already-in-use') {
+          // we can return message as we want
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.message ?? 'Authentication failed')));
+      }
+
+    }
   }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,63 +65,65 @@ class _AuthScreenState extends State<AuthScreen> {
                   child: Padding(
                     padding: EdgeInsets.all(16),
                     child: Form(
-                      key: _form,
+                        key: _form,
                         child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email address',
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value){
-                            if (value == null || value.trim().isEmpty || !value.contains('@')) {
-                              return 'Please enter a valid email address';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            enteredEmail = value!;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password',
-                          ),
-                          obscureText: true,
-                          validator: (value){
-                            if (value == null || value.trim().length <6) {
-                              return 'Password must be at least characters long';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            enteredPassword = value!;
-                          },
-                        ),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        ElevatedButton(
-                            onPressed: _submit,
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: Theme.of(context)
-                                    .colorScheme
-                                    .primaryContainer),
-                            child: Text(_isLogin ? 'Log in' : 'Sign Up')),
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                _isLogin = !_isLogin;
-                              });
-                            },
-                            child: Text(_isLogin
-                                ? 'Create an account'
-                                : 'Already have an account? Sign in')),
-                      ],
-                    )),
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Email address',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter a valid email address';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                enteredEmail = value!;
+                              },
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value == null || value.trim().length < 6) {
+                                  return 'Password must be at least characters long';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                enteredPassword = value!;
+                              },
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            ElevatedButton(
+                                onPressed: _submit,
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .primaryContainer),
+                                child: Text(_isLogin ? 'Log in' : 'Sign Up')),
+                            TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _isLogin = !_isLogin;
+                                  });
+                                },
+                                child: Text(_isLogin
+                                    ? 'Create an account'
+                                    : 'Already have an account? Sign in')),
+                          ],
+                        )),
                   ),
                 ),
               )
